49c1a8d915014882aee219ad77bc9fd2cb63cb74
Anna Pawl <annampawl@gmail.com>
2019-02-10 21:07:11 -0800

    simplest local web app up and running

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 # FLASK QUICKSTART
-
 from flask import Flask
+from flask import render_template
+import dictionary_words
+
 # Instance of Flask class
 app = Flask(__name__)
 
 # Tell Flask what url should trigger function, return message
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/testing')
+def tweets():
+    r
